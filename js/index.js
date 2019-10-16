d2381289e2bf8b584cfd9ae63162445ceb73879c
Sergey Malinovsky <malinovsky.exceedteam@gmail.com>
2019-10-16 18:44:01 +0300

    work build #1

--- js/index.js
+++ js/index.js
@@ -2,28 +2,58 @@
 const template = (_id, _state, _text) => {
     let tmp_text = _text;
     let checkbox_checked = '';
+/*
+    let elem = document.getElementsByClassName(`completeTask${_id}`)[0];
+    console.log(_state);
+    if(elem != undefined){
+        if(_state == true){
+            console.log("Checked working!");
+
+            elem.style.setProperty("text-decoration", "line-through");
+        }else{
+            
+            elem.style.setProperty("text-decoration", "none");
+        }
+    }
+  */ 
+    let check = _state ? 'checked' : '';
+    
     return `<li itemid='${_id}'>
                 <div class='item'>
-                    <input type='checkbox' ${checkbox_checked} class='ch-box-complete-task${_id}' id='${_id}' onclick='changeStateTask(${_id})'> 
-                        <label for="${_id}" class='completeTask${_id}' >${tmp_text}</label>
-<style>
-.completeTask${_id} {
-    text-decoration: none;
-}
-</style>
+                    <input type='checkbox' ${check} ${checkbox_checked} class='ch-box-complete-task${_id}' id='${_id}' onclick='changeStateTask(${_id})'> 
+                        <label for="${_id}" class='completeTask${_id} completeTask'>${_text}</label>
+
                     <a href='#'><img src='img/delete_icon.jpg' alt='Delete' onclick='DeleteTask(${_id})'></a>
                     </div>
             </li>`;
 }
 
+function changeStateLabel(_id, _state){
+    let elem = document.getElementsByClassName(`completeTask${_id}`)[0];
+    console.log(_id);
+    console.log(elem);
+    if(elem){
+        if(_state == true){
+            console.log("Checked working!");
+            elem.classList.add('cross');
+        }else{
+            elem.classList.remove('cross');
+        }   
+    }
+    
+}
+/*
+<!-- <style>
+.completeTask${_id} {
+    text-decoration: none;
+}
+</style> -->
+*/
 // ========================================================================================
 
 let items = [
-    {id: 0, text: "FirstItem",  state: false},
-    {id: 1, text: "SecondItem", state: false},
-    {id: 2, text: "ThirdItem",  state: false},
     
-]
+];
 
 let item_li = document.createElement('li');
 let template_li = document.createElement('ul');
@@ -34,37 +64,73 @@ function addTask(){
     if(_text[0].value){
         console.log(_text[0].value);
 
-        items.push({id: items.length, text: _text[0].value, state: false});
+        let itemId;
+        if (items.length > 0) {
+            itemId = items[items.length - 1].id+1;
+        } else {
+            itemId = 1;
+        }
 
-        template_li.innerHTML += template(items.length, false, _text[0].value);
+        
+        //if(items == null){
+            //items = [{id: 0, text: _text[0].value, state: false}]
+        //}else{
+            items.push({id: itemId, text: _text[0].value, state: false});
+        //}
+        console.log(items);
+        addLocalStorage();
+
+        template_li.innerHTML += template(itemId, false, _text[0].value);
+
+        _text[0].value = '';
+
+        let emptyElem = document.getElementById('empty');
+
+        if(emptyElem){
+            emptyElem.remove();
+        }
+        
     }else{
         alert("Поле не может быть пустым!");
     }
-    addLocalStorage();
+    
 }
 
-function addViewTask(_items=items, v=items.length){
+function sasasaddViewTask(_items, v){
     template_li.innerHTML += template(_items[v].id, items[v].state, items[v].text);
     article.append(template_li);
+    changeStateLabel(_items[v].id, items[v].state);
 }
 
 function DeleteTask(_id){
     console.log(_id);
+
     let item = document.querySelector('li[itemid="'+_id+'"]');
+    item.remove();
+
     console.log(item);
 
-    for(i in items){
-        if(items[i].id == _id){
-            if(items.length == 1) i=items.length;
-            items.splice(i, 1);
-            console.log("Found!");
-            break;
-        }
+    items = items.filter(i => i.id != _id);
+    //for(i in items){
+        //if(items[i].id == _id){
+
+
+
+            //if(items.length == 1){
+                //for(i in items) items.splice(i, 1);
+            //}
+            //items.splice(i, 1);
+            //console.log(items.length);
+            //break;
+        //}
+        
+    //}
+    if(items.length == 0){
+        template_li.innerHTML = "<div class='item' id='empty'><h3 align='center'>Empty...</h3></div>";
     }
 
     addLocalStorage();
-
-    item.remove();
+   
 }
 
 function changeStateTask(_id){
@@ -73,56 +139,73 @@ function changeStateTask(_id){
     let elem = document.getElementsByClassName(`completeTask${_id}`)[0];
     console.log(_id+" "+state_checkbox+" "+elem);
 
-    for(i in items){
+    for(let i=0; i<items.length; i++){
         if(items[i].id == _id){
-            items[i].state == state_checkbox;
-            console.log("Found!");
+            items[i].state = state_checkbox;
+            //console.log("Found!");
+            console.log(items);
             break;
         }
     }
 
+    console.log(items);
+    addLocalStorage();
+
     //let rmText = document.
     if(state_checkbox){
-        elem.style.setProperty("text-decoration", "line-through");
+        elem.classList.add('cross');
     }else{
-        elem.style.setProperty("text-decoration", "none");
+        elem.classList.remove('cross');
     }
 }
 
 // ============================== Local Storage Functions =================================
 
 function addLocalStorage(){
-    let jsonItems = JSON.stringify(items);
-    localStorage.setItem("local_data", jsonItems);
+    if(items != null){
+        let jsonItems = JSON.stringify(items);
+        localStorage.setItem("local_data", jsonItems);
+    }else{
+        console.log(items+' is NULL!');
+    }
+    
 }
 
 function loadLocalStorage(){
     let _loadItems = JSON.parse(localStorage.getItem("local_data"));
-    console.log(_loadItems);
+    //console.log(_loadItems);
     return _loadItems;
 }
 
 // ========================================================================================
 function viewItems(){
-
-    let tmp_item_name = '';
-    let checkbox_checked = ''
-
     items = loadLocalStorage();
     //console.log(_items);
-    item_li.innerHTML = '';
+    template_li.innerHTML = '';
+
+    console.log(items);
 
     if(items.length == 0){
-        item_li.innerHTML = "<div class='item'><h3 align='center'>Empty...</h3></div>"
+        console.log("Empty list of tasks!");
+        template_li.innerHTML = "<div class='item' id='empty'><h3 align='center'>Empty...</h3></div>";
     }else{
         for(let i=0; i<items.length; i++){
+            //console.log(items.length);
+            //console.log(items[i].id);
+            //addViewTask(items, items[i].id);
 
-            addViewTask(items, i);
-            
-        }    
+            template_li.innerHTML += template(items[i].id, items[i].state ,items[i].text);
+            article.append(template_li);
+            changeStateLabel(items[i].id, items[i].state);
+        }  
+        addLocalStorage();  
     }
 
-    addLocalStorage();
+    
+    
+    
 }
+
+
 
 viewItems();
